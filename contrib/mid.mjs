0b0b52813acb225b9159febf8114c54d1ff913cb
Tomas Carnecky <tomc@caurea.org>
2021-08-05 21:57:51 +0200

    ...

---
+++ contrib/mid.mjs
@@ -0,0 +1,11 @@
+import { customAlphabet } from 'nanoid'
+
+const alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+do {
+  const id = customAlphabet(alphabet, 30)()
+  if (id.match(/^[A-Za-z]/)) {
+    process.stdout.write(id)
+    process.exit()
+  }
+} while (true)
