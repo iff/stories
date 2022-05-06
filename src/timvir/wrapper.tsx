034018b10d6627c333f7177960e3617a2ca4ffaf
Tomas Carnecky <tomc@caurea.org>
2022-05-06 23:45:36 +0200

    Bump timvir

--- src/timvir/wrapper.tsx
+++ src/timvir/wrapper.tsx
@@ -1,11 +1,11 @@
-import { Page } from "@timvir/core";
+import { Page } from "timvir/core";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import * as React from "react";
 import Head from "next/head";
 import toc from "./toc";
 import { Components } from "@mdx-js/react/lib/index";
-import { Code } from "@timvir/blocks";
+import { Code } from "timvir/blocks";
 
 const mdxComponents: Components = {
   pre: function pre(props: any) {
@@ -18,8 +18,7 @@ export default function Wrapper({ children }: { children?: React.ReactNode }) {
   return (
     <>
       <Head>
-        <link href="https://unpkg.com/@timvir/core@0.1.37/styles.css" rel="stylesheet" />
-        <link href="https://unpkg.com/@timvir/blocks@0.1.37/styles.css" rel="stylesheet" />
+        <link href="https://unpkg.com/timvir@0.1.44/styles.css" rel="stylesheet" />
       </Head>
 
       <Page location={useRouter()} Link={Link} toc={toc} mdxComponents={mdxComponents}>
