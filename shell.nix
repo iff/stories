dce377b28b653f9b5a96cf8f2baa4239ebbfd672
Tomas Carnecky <tomc@caurea.org>
2022-05-28 18:54:12 +0200

    Prettify shell

--- shell.nix
+++ shell.nix
@@ -1,10 +1,27 @@
 let
   pkgs = import <nixpkgs> {};
 
+  banner = pkgs.writeShellScriptBin "banner" ''
+    clear
+
+    ${pkgs.figlet}/bin/figlet -c stories
+  '';
+
+  dev = pkgs.writeShellScriptBin "dev" ''
+    npm run dev
+  '';
+
 in pkgs.mkShell {
   buildInputs = [
     pkgs.nodejs-16_x
-
     pkgs.google-cloud-sdk
+
+    dev
   ];
+
+  shellHook = ''
+    if [ -n "$PS1" ]; then
+      ${banner}/bin/banner
+    fi
+  '';
 }
