bf57f9ec24c2838aefbcb07566509840eb2a7ddb
Tomas Carnecky <tomc@caurea.org>
2022-11-21 14:26:17 +0100

    Use custom Linaria-Next.js integration

--- next.config.js
+++ next.config.js
@@ -1,38 +1,40 @@
-import withLinaria from "next-linaria";
 import withMDX from "@next/mdx";
 import { remarkPlugin } from "@timvir/mdx";
+import withPlugins from "next-compose-plugins";
+import { withLinaria } from "./next/withLinaria.js";
 
-export default withLinaria(
+const plugins = [
+  withLinaria({
+    cacheDirectory: "./.next/cache/linaria",
+  }),
   withMDX({
     extension: /\.mdx?$/,
     options: {
       providerImportSource: "@mdx-js/react",
       remarkPlugins: [remarkPlugin],
     },
-  })({
-    reactStrictMode: true,
+  }),
+];
 
-    linaria: {
-      cacheDirectory: "./.next/cache/linaria",
-    },
+export default withPlugins(plugins, {
+  reactStrictMode: true,
 
-    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 
-    images: {
-      domains: ["storage.googleapis.com", "web-4n62l3bdha-lz.a.run.app"],
-    },
+  images: {
+    domains: ["storage.googleapis.com", "web-4n62l3bdha-lz.a.run.app"],
+  },
 
-    env: {
-      API: process.env.API,
-    },
+  env: {
+    API: process.env.API,
+  },
 
-    async rewrites() {
-      return [
-        {
-          source: "/feed",
-          destination: "/api/feed",
-        },
-      ];
-    },
-  })
-);
+  async rewrites() {
+    return [
+      {
+        source: "/feed",
+        destination: "/api/feed",
+      },
+    ];
+  },
+});
