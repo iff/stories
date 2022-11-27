88804450118713b9679e0f08d93f3893058be8b6
Tomas Carnecky <tomc@caurea.org>
2022-11-27 20:18:38 +0100

    fix story og image

--- content/where-i-was-meant-to-be/image.tsx
+++ content/where-i-was-meant-to-be/image.tsx
@@ -1,11 +1,15 @@
 import NextImage from "next/legacy/image";
 import * as React from "react";
-import { importBlob } from "../../image.macro";
 
 export default function Page() {
   return (
     <div style={{ width: "100vw", height: "100vh", display: "grid" }}>
-      <NextImage alt="" src={importBlob("2ZfTSs9o51poWKLtqQ2X9Fo7jgKE7dK8g4CXKybZxQB2").src} layout="fill" objectFit="cover" />
+      <NextImage
+        alt=""
+        src={`${process.env.API}/serve/2ZfTSs9o51poWKLtqQ2X9Fo7jgKE7dK8g4CXKybZxQB2`}
+        layout="fill"
+        objectFit="cover"
+      />
     </div>
   );
 }
