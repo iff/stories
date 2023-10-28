5cebe46c0d6b8bf0814f01530ab3240297a2bffc
Tomas Carnecky <tomc@caurea.org>
2023-10-29 00:01:03 +0200

    Remove type annotation on mdxComponents

--- src/timvir/wrapper.tsx
+++ src/timvir/wrapper.tsx
@@ -4,10 +4,9 @@ import { useRouter } from "next/router";
 import * as React from "react";
 import Head from "next/head";
 import toc from "./toc";
-import { Components } from "@mdx-js/react/lib/index";
 import { Code } from "timvir/blocks";
 
-const mdxComponents: Components = {
+const mdxComponents = {
   pre: function pre(props: any) {
     const [, language = "markdown"] = (props.className || "").match(/^language-(.*)$/) || [];
     return <Code language={language}>{props.children.props.children}</Code>;
