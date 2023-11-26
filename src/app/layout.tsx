e184a565d16d5ca323328776b71d9a7d5a387d61
Tomas Carnecky <tomc@caurea.org>
2023-11-26 23:59:50 +0100

    Add feed link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,15 @@ export const viewport: Viewport = {
   viewportFit: "cover",
 };
 
+const baseUrl = process.env.NODE_ENV === "production" ? `https://stories.caurea.org` : "http://localhost:3000";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <script type="module" dangerouslySetInnerHTML={{ __html: themeDetector }} />
+
+        <link rel="alternate" type="application/rss+xml" title="Stories by Tomáš Čarnecky" href={`${baseUrl}/feed`} />
       </head>
 
       <body>{children}</body>
