938ba1bbb33f18bea645021a9d8acecc02b6d844
Yves Ineichen <iff@yvesineichen.com>
2024-02-10 14:40:48 +0100

    layout: changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../style.linaria.global";
 
 export const metadata: Metadata = {
-  title: "Stories by Tomáš Čarnecky",
+  title: "stories by iff",
 };
 
 export const viewport: Viewport = {
@@ -14,14 +14,14 @@ export const viewport: Viewport = {
   viewportFit: "cover",
 };
 
-const baseUrl = process.env.NODE_ENV === "production" ? `https://stories.caurea.org` : "http://localhost:3000";
+const baseUrl = process.env.NODE_ENV === "production" ? `https://iff.io` : "http://localhost:3000";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.png" />
-        <link rel="alternate" type="application/rss+xml" title="Stories by Tomáš Čarnecky" href={`${baseUrl}/feed`} />
+        <link rel="alternate" type="application/rss+xml" title="Yves Ineichen" href={`${baseUrl}/feed`} />
       </head>
 
       <body>{children}</body>
