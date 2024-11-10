d0d709a826433d70650aacb71fff950f85a7a075
Tomáš Čarnecký <tomc@caurea.org>
2024-11-10 17:16:49 +0100

    Deployment Verification

--- urnerys/shared.ts
+++ urnerys/shared.ts
@@ -7,21 +7,21 @@ export function sanitizeTitle(s: string): string {
     .toLowerCase();
 }
 
-export const build = process.env.GITHUB_RUN_ID || sanitizeTitle(new Date().toISOString());
+export const build = process.env.GITHUB_RUN_ID
+  ? `github-run-${process.env.GITHUB_RUN_ID}-${process.env.GITHUB_RUN_ATTEMPT}`
+  : sanitizeTitle(new Date().toISOString());
 
 export async function uploadImage(page, set, title, formula) {
   await page.addStyleTag({ content: "img { display: none !important; }" });
   const buffer = await page.screenshot({ fullPage: true });
 
   const formData = new FormData();
-  formData.set("project", "stories");
-  formData.set("build", build);
-  formData.set("set", set);
+  formData.set("collection", set);
   formData.set("snapshot", sanitizeTitle(title));
   formData.set("formula", formula);
   formData.set("payload", new File([buffer], "screenshot.png", { type: "image/png" }));
 
-  await fetch(`https://${process.env.URNERYS}/rpc/uploadImage`, {
+  await fetch(`https://${process.env.URNERYS}/api/v1/projects/stories/builds/${build}/images`, {
     method: "POST",
     body: formData,
   });
