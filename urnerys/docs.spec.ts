62d3310baa6df745fe48e5d079df3d60bc1509d8
Tomáš Čarnecký <tomc@caurea.org>
2024-12-07 11:56:31 +0100

    Fix integration with Urnerys

--- urnerys/docs.spec.ts
+++ urnerys/docs.spec.ts
@@ -16,22 +16,20 @@ for (const url of ["/docs/components/Image", "/docs/components/Group", "/docs/co
     await page.goto(url, { waitUntil: "domcontentloaded" });
     await page.waitForTimeout(2000);
 
-    const elements = await page.$$(".r1nql2f9");
+    const elements = await page.$$(".timvir-b-Exhibit");
     for (const [index, element] of elements.entries()) {
       const buffer = await element.screenshot();
 
-      const childElement = await element.$(".cf43jjx");
+      const childElement = await element.$(".timvir-b-Exhibit-caption");
       const innerText = (await childElement?.innerText()) ?? `${index}`;
 
       const formData = new FormData();
-      formData.set("project", "stories");
-      formData.set("build", build);
-      formData.set("set", "docs/exhibits");
+      formData.set("collection", "docs/exhibits");
       formData.set("snapshot", sanitizeTitle(innerText));
       formData.set("formula", "none");
       formData.set("payload", new File([buffer], "screenshot.png", { type: "image/png" }));
 
-      await fetch(`https://${process.env.URNERYS}/rpc/uploadImage`, {
+      await fetch(`https://${process.env.URNERYS}/api/v1/projects/stories/builds/${build}/images`, {
         method: "POST",
         body: formData,
       });
