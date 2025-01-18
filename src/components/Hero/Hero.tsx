7280e2079e0218802a0fa6fa7fb32bac226f70c4
Tomáš Čarnecký <tomc@caurea.org>
2025-01-18 15:21:13 +0100

    Use StyleX in Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,9 @@
+import * as stylex from "@stylexjs/stylex";
+import * as React from "react";
+
 import { importBlob } from "@/cms";
 import { Brandmark } from "@/components/Brandmark";
-import { css, cx } from "@linaria/core";
 import Image from "next/image";
-import * as React from "react";
 
 /**
  * The underlying DOM element which is rendered by this component.
@@ -12,33 +13,25 @@ const Root = "div";
 interface Props extends React.ComponentPropsWithoutRef<typeof Root> {}
 
 async function Hero(props: Props) {
-  const { className, ...rest } = props;
+  const { ...rest } = props;
 
   const blob = await importBlob("DtmNcLbCUqzbXVfgQSowJeJZTTs9UztyYkupTzAQzbCf");
 
   return (
-    <Root className={cx(classes.root, className)} {...rest}>
-      <div className={classes.image}>
-        <Image
-          alt=""
-          src={blob.asImage.url}
-          fill
-          sizes="100vw"
-          style={{
-            objectFit: "cover",
-          }}
-        />
+    <Root {...stylex.props(styles.root)} {...rest}>
+      <div {...stylex.props(styles.image)}>
+        <Image alt="" src={blob.asImage.url} fill sizes="100vw" {...stylex.props(styles.img)} />
         <div
-          className="sqip"
+          {...stylex.props(styles.sqip)}
           style={{
             backgroundImage: `url(${blob.asImage.placeholder.url})`,
           }}
         />
       </div>
 
-      <div className={classes.box}>
+      <div {...stylex.props(styles.box)}>
         <Brandmark size="large" />
-        <div className={classes.lead}>…for nothing remains of us but the vibrations we leave behind.</div>
+        <div {...stylex.props(styles.lead)}>…for nothing remains of us but the vibrations we leave behind.</div>
       </div>
     </Root>
   );
@@ -46,83 +39,80 @@ async function Hero(props: Props) {
 
 export default Hero;
 
-const classes = {
-  root: css`
-    height: 100vh;
-    height: 100svh;
-    display: grid;
-    place-items: center;
-    justify-items: center;
-    isolation: isolate;
-  `,
+const styles = stylex.create({
+  root: {
+    height: ["100vh", "100svh"],
+    display: "grid",
+    placeItems: "center",
+    justifyItems: "center",
+    isolation: "isolate",
+  },
 
-  box: css`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  box: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
 
-    grid-column: 1 / span 1;
-    grid-row: 1 / span 1;
-  `,
+    gridColumn: "1 / span 1",
+    gridRow: "1 / span 1",
+  },
 
-  lead: css`
-    background: #18191b;
-    color: white;
-    text-align: center;
+  lead: {
+    background: "#18191b",
+    color: "white",
+    textAlign: "center",
 
-    padding: 0.7em 1em;
+    padding: "0.7em 1em",
 
-    font-size: clamp(20px, 1.5vw, 36px);
-    line-height: 1.3;
+    fontSize: "clamp(20px, 1.5vw, 36px)",
+    lineHeight: 1.3,
 
-    max-width: min(30ch, 80vw);
-    margin-top: 2em;
-  `,
+    maxWidth: "min(30ch, 80vw)",
+    marginTop: "2em",
+  },
 
-  image: css`
-    position: relative;
-    z-index: -1;
-    width: 100vw;
-    height: 100vh;
-    height: 100svh;
+  image: {
+    position: "relative",
+    zIndex: -1,
+    width: "100vw",
+    height: ["100vh", "100svh"],
 
-    grid-column: 1 / span 1;
-    grid-row: 1 / span 1;
+    gridColumn: "1 / span 1",
+    gridRow: "1 / span 1",
 
-    &:after {
-      display: block;
-      content: "";
-      position: absolute;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      right: 0;
-      background: radial-gradient(circle, transparent 70%, black 200%);
-      z-index: 2;
-    }
+    "::after": {
+      display: "block",
+      content: '""',
+      position: "absolute",
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      background: "radial-gradient(circle, transparent 70%, black 200%)",
+      zIndex: 2,
+    },
 
-    .sqip {
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      pointer-events: none;
-      transition: opacity 0.8s ease-out 0.2s;
-      background-size: cover;
-      background-position: 50% 50%;
+    "@media (min-width: 720px)": {
+      width: "calc(100vw - 176px)",
+      height: ["calc(100vh - 140px)", "calc(100svh - 140px)"],
+    },
+  },
 
-      z-index: 1;
-    }
+  sqip: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    pointerEvents: "none",
+    backgroundSize: "cover",
+    backgroundPosition: "50% 50%",
 
-    & img {
-      z-index: 2;
-    }
+    zIndex: 1,
+  },
 
-    @media (min-width: 720px) {
-      width: calc(100vw - 176px);
-      height: calc(100vh - 140px);
-      height: calc(100svh - 140px);
-    }
-  `,
-};
+  img: {
+    zIndex: 2,
+    objectFit: "cover",
+  },
+});
