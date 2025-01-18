8e284138ab97de86a5ed11e95a4ce042d4b563d6
Tomáš Čarnecký <tomc@caurea.org>
2025-01-18 16:20:12 +0100

    Use StyleX in Lightbox component

--- src/components/Lightbox/Lightbox.tsx
+++ src/components/Lightbox/Lightbox.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { css, cx } from "@linaria/core";
+import * as stylex from "@stylexjs/stylex";
+import * as React from "react";
+
 import Link, { LinkProps } from "next/link";
 import { useRouter } from "next/navigation";
-import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as Icons from "react-feather";
 
@@ -24,7 +25,7 @@ interface Props extends React.ComponentPropsWithoutRef<typeof Root> {
 function Lightbox(props: Props, ref: React.ForwardedRef<React.ComponentRef<typeof Root>>) {
   const router = useRouter();
 
-  const { onClose, caption, prev, next, className, children, ...rest } = props;
+  const { onClose, caption, prev, next, children, ...rest } = props;
 
   /*
    * Attach event handlers to 'document' to handle keyboard shortcuts.
@@ -61,26 +62,26 @@ function Lightbox(props: Props, ref: React.ForwardedRef<React.ComponentRef<typeo
   }, [router, onClose, prev, next]);
 
   const el = (
-    <Root ref={ref} className={cx(className, classes.root)} {...rest}>
-      <div className={classes.top}>
-        <Nav className={classes.close} action={onClose}>
-          <Icons.X />
+    <Root ref={ref} {...stylex.props(styles.root)} {...rest}>
+      <div {...stylex.props(styles.top)}>
+        <Nav {...stylex.props(styles.close)} action={onClose}>
+          <Icons.X style={{ display: "block" }} />
         </Nav>
       </div>
 
-      <div className={classes.center}>
+      <div {...stylex.props(styles.center)}>
         {children}
 
-        <Nav action={prev} className={classes.prev}>
-          <Icons.ArrowLeft />
+        <Nav action={prev} {...stylex.props(styles.prev)}>
+          <Icons.ArrowLeft style={{ display: "block" }} />
         </Nav>
 
-        <Nav action={next} className={classes.next}>
-          <Icons.ArrowRight />
+        <Nav action={next} {...stylex.props(styles.next)}>
+          <Icons.ArrowRight style={{ display: "block" }} />
         </Nav>
       </div>
 
-      <div className={classes.caption}>{caption}</div>
+      <div {...stylex.props(styles.caption)}>{caption}</div>
     </Root>
   );
 
@@ -93,89 +94,86 @@ function Lightbox(props: Props, ref: React.ForwardedRef<React.ComponentRef<typeo
 
 export default React.forwardRef(Lightbox);
 
-const classes = {
-  root: css`
-    position: fixed;
-    inset: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: #18191b;
-    color: white;
-    display: flex;
-    flex-direction: column;
-  `,
+const styles = stylex.create({
+  root: {
+    position: "fixed",
+    inset: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    background: "#18191b",
+    color: "white",
+    display: "flex",
+    flexDirection: "column",
+  },
 
-  top: css`
-    z-index: 3;
-    min-height: 64px;
-    flex: 64px 0 1;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    padding: env(safe-area-inset-top) env(safe-area-inset-right) 0 env(safe-area-inset-left);
-  `,
+  top: {
+    zIndex: 3,
+    minHeight: 64,
+    flex: "64px 0 1",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: "env(safe-area-inset-top) env(safe-area-inset-right) 0 env(safe-area-inset-left)",
+  },
 
-  close: css`
-    padding: 12px;
-    cursor: pointer;
-    color: inherit;
+  close: {
+    padding: 12,
+    cursor: "pointer",
+    color: "inherit",
+  },
 
-    svg {
-      display: block;
-    }
-  `,
+  center: {
+    flexGrow: 1,
+    position: "relative",
+  },
 
-  center: css`
-    flex-grow: 1;
-    position: relative;
-  `,
+  prev: {
+    zIndex: 2,
+    position: "fixed",
+    paddingRight: 32,
+    left: 0,
+    top: 0,
+    bottom: 0,
+    width: "calc(80px + env(safe-area-inset-left))",
+    paddingLeft: "env(safe-area-inset-left)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    transition: "opacity 0.2s",
+    textDecoration: "none",
+    color: "inherit",
+  },
 
-  prev: css`
-    z-index: 2;
-    position: fixed;
-    padding-right: 32px;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    width: calc(80px + env(safe-area-inset-left));
-    padding-left: env(safe-area-inset-left);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    transition: opacity 0.2s;
-    text-decoration: none;
-    color: inherit;
-  `,
-  next: css`
-    z-index: 2;
-    position: fixed;
-    padding-left: 32px;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    width: calc(80px + env(safe-area-inset-right));
-    padding-right: env(safe-area-inset-right);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    transition: opacity 0.2s;
-    text-decoration: none;
-    color: inherit;
-  `,
+  next: {
+    zIndex: 2,
+    position: "fixed",
+    paddingLeft: 32,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    width: "calc(80px + env(safe-area-inset-right))",
+    paddingRight: "env(safe-area-inset-right)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    transition: "opacity 0.2s",
+    textDecoration: "none",
+    color: "inherit",
+  },
 
-  caption: css`
-    min-height: 64px;
-    padding: 24px 0 max(24px, env(safe-area-inset-bottom));
-    text-align: center;
-    width: 100%;
-    font-size: 0.9em;
-    font-style: italic;
-  `,
-};
+  caption: {
+    minHeight: 64,
+    padding: "24px 0 max(24px, env(safe-area-inset-bottom))",
+    textAlign: "center",
+    width: "100%",
+    fontSize: "0.9em",
+    fontStyle: "italic",
+  },
+});
 
 function Nav(props: { action?: LinkProps | (() => void); className?: string; children?: React.ReactNode }) {
   const { action, ...rest } = props;
