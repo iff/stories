9bceb7a629a66a1d3f7bd6d0c60dde8978fca784
Tomáš Čarnecký <tomc@caurea.org>
2025-01-18 16:29:23 +0100

    Use StyleX in Story Heading component

--- src/components/Story/internal/components/Heading.tsx
+++ src/components/Story/internal/components/Heading.tsx
@@ -1,37 +1,42 @@
-import { css, cx } from "@linaria/core";
+import * as stylex from "@stylexjs/stylex";
 import * as React from "react";
+import { cx } from "@linaria/core";
 
 interface Props {
   children?: React.ReactNode;
 }
 
 export default function Heading_(props: Props) {
+  /*
+   * Hack to allow className to be appended to. Should be removed once
+   * we migrate fully to StyleX.
+   */
+  const s = stylex.props(styles.root);
+
   return (
-    <div
-      className={cx(
-        "noLayout",
-        css`
-          grid-column: lex / rc;
-        `
-      )}
-    >
-      <h2
-        className={css`
-          display: inline-block;
-
-          margin: 2em 0 1em;
-          padding: 0.55em 0.7em 0.4em;
-
-          background: black;
-          color: white;
-
-          font-size: clamp(32px, 3.5vw, 80px);
-          line-height: 1.2;
-          font-weight: 900;
-          letter-spacing: 0.09em;
-        `}
-        {...props}
-      />
+    <div {...s} className={cx("noLayout", s.className)}>
+      <h2 {...stylex.props(styles.heading)} {...props} />
     </div>
   );
 }
+
+const styles = stylex.create({
+  root: {
+    gridColumn: "lex / rc",
+  },
+
+  heading: {
+    display: "inline-block",
+
+    margin: "2em 0 1em",
+    padding: "0.55em 0.7em 0.4em",
+
+    background: "black",
+    color: "white",
+
+    fontSize: "clamp(32px, 3.5vw, 80px)",
+    lineHeight: 1.2,
+    fontWeight: 900,
+    letterSpacing: "0.09em",
+  },
+});
