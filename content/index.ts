2d1ea2b4d336f9c22b6b19129bd5bca05e8f27f4
Yves Ineichen <iff@yvesineichen.com>
2025-11-12 20:06:54 +0100

    content: slimmer index

--- content/index.ts
+++ content/index.ts
@@ -3,11 +3,12 @@ import * as React from "react";
 
 export const site = {
   title: "Lenses",
-  byline: "sometimes the lense with which I perceive the world is a physical device attached to a camera",
+  byline:
+    "sometimes the lense with which I perceive the world is a physical device attached to a camera",
 
   author: {
     name: "Yves Ineichen",
-    email: "y@y.io",
+    email: "y@iff.io",
   },
 };
 
@@ -33,21 +34,6 @@ interface Story {
    */
   date: [Date, Date];
 
-  /**
-   * The primary image for the story. This is used in the teaser on the home page,
-   * in the header on the story page, and as the Open Graph image.
-   */
-  image: string;
-
-  teaser: {
-    text: string;
-
-    /**
-     * At least one additional image to be used on the home page teaser.
-     */
-    images: [string, ...string[]];
-  };
-
   body: {
     Component: React.ComponentType;
   };
@@ -61,14 +47,10 @@ export const stories: Story[] = [
     publishedAt: new Date(Date.parse("2020-01-01")),
 
     title: "Hawaii",
-    date: [new Date(Date.parse("2016-11-15")), new Date(Date.parse("2016-12-01"))],
-
-    image: "GzkGi2tBqxRFQ3efnr5Kxa64tX56hb4fbnK5tTWM5a9k",
-
-    teaser: {
-      text: "fairytale island",
-      images: ["GNm6m1gnH9TiRwYrrgsmg3HdVRjEWDZTW1xAYmoJuHNR"],
-    },
+    date: [
+      new Date(Date.parse("2016-11-15")),
+      new Date(Date.parse("2016-12-01")),
+    ],
 
     body: {
       /**
@@ -84,14 +66,10 @@ export const stories: Story[] = [
     publishedAt: new Date(Date.parse("2020-01-01")),
 
     title: "Amsterdam",
-    date: [new Date(Date.parse("2017-05-20")), new Date(Date.parse("2017-06-01"))],
-
-    image: "47xXYVZEjAVVyEqYs7TpphRpE52WKZ7nVXoqJPT67iX3",
-
-    teaser: {
-      text: "smombie life",
-      images: ["Gh83ou9cqktKK9PBZLukuDviJ7Dvfqf1ep5zcZ3KA5kn"],
-    },
+    date: [
+      new Date(Date.parse("2017-05-20")),
+      new Date(Date.parse("2017-06-01")),
+    ],
 
     body: {
       /**
@@ -107,14 +85,10 @@ export const stories: Story[] = [
     publishedAt: new Date(Date.parse("2020-01-01")),
 
     title: "Marocco",
-    date: [new Date(Date.parse("2019-01-31")), new Date(Date.parse("2019-02-10"))],
-
-    image: "6hkdp7oJ9g8qqzGcP9AAXrv9xk6S9GTUPB42J1YAYXeX",
-
-    teaser: {
-      text: "colorful remoteness",
-      images: ["2XeYAL9caXND6XJB1988dfnS4ZWVkc94aFM5SSNoeyYw"],
-    },
+    date: [
+      new Date(Date.parse("2019-01-31")),
+      new Date(Date.parse("2019-02-10")),
+    ],
 
     body: {
       /**
@@ -130,14 +104,10 @@ export const stories: Story[] = [
     publishedAt: new Date(Date.parse("2020-01-01")),
 
     title: "Snowline",
-    date: [new Date(Date.parse("2021-03-06")), new Date(Date.parse("2021-03-06"))],
-
-    image: "C673XbvFp8M5k899he4QNmj919yu5uMsBbckNfNyUVqT",
-
-    teaser: {
-      text: "unexpected separation",
-      images: ["9ECapbRAJzBu2BoZ6UG47EzRxDJFgAdr9xiTPF5VTcst"],
-    },
+    date: [
+      new Date(Date.parse("2021-03-06")),
+      new Date(Date.parse("2021-03-06")),
+    ],
 
     body: {
       /**
@@ -153,14 +123,10 @@ export const stories: Story[] = [
     publishedAt: new Date(Date.parse("2020-01-01")),
 
     title: "Bavona",
-    date: [new Date(Date.parse("2020-01-01")), new Date(Date.parse("2024-02-10"))],
-
-    image: "HMzyN2b5oADqoKsK29WZjxebYWWUWhZ7PZQ3HQyzn8WV",
-
-    teaser: {
-      text: "second home",
-      images: ["DK7FoWh3M9J1Nwk7GFGjcYoCMwpsDcJzZo8DZtK9wMwV"],
-    },
+    date: [
+      new Date(Date.parse("2020-01-01")),
+      new Date(Date.parse("2024-02-10")),
+    ],
 
     body: {
       /**
@@ -176,14 +142,10 @@ export const stories: Story[] = [
     publishedAt: new Date(Date.parse("2020-01-01")),
 
     title: "Bergell",
-    date: [new Date(Date.parse("2020-01-01")), new Date(Date.parse("2024-02-10"))],
-
-    image: "BCctiLtVp3AheJNa1RqSZ32eMAtjRvJ6ppYMESJ845x8",
-
-    teaser: {
-      text: "escaping lockdown",
-      images: [""],
-    },
+    date: [
+      new Date(Date.parse("2020-01-01")),
+      new Date(Date.parse("2024-02-10")),
+    ],
 
     body: {
       /**
@@ -199,14 +161,10 @@ export const stories: Story[] = [
     publishedAt: new Date(Date.parse("2020-01-01")),
 
     title: "New Zealand",
-    date: [new Date(Date.parse("2020-01-01")), new Date(Date.parse("2024-02-10"))],
-
-    image: "8Cfadtq1pQ2XgLn55skdb2CoawepiJF1kF9HwUZW8T3r",
-
-    teaser: {
-      text: "escaping lockdown",
-      images: [""],
-    },
+    date: [
+      new Date(Date.parse("2020-01-01")),
+      new Date(Date.parse("2024-02-10")),
+    ],
 
     body: {
       /**
